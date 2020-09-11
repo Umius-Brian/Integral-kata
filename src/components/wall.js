59e552e20012bc593ec7385d6dc500511bce0974
Brian Bui <59684534+Umius-Brian@users.noreply.github.com>
2020-09-11 14:27:34 -0700

    Update with follower db call

--- src/components/wall.js
+++ src/components/wall.js
@@ -1,10 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import Moment from 'react-moment';
 
 export default function Wall() {
+  const [followersPosts, setfollowersPosts] = useState([]);
+
+  async function dbUsers() {
+    const result = await axios('http://localhost:5000/followers');
+    setfollowersPosts(result.data);
+  }
+
+  useEffect(() => { dbUsers() }, []); 
   return (
     <div>
-      <p>Wall List</p>
+      <h3>Follower Posts</h3>
+      <br />
+      <ul>
+        {followersPosts.map(follower => (
+          <li key={follower.objectID}>
+            <a href={follower.url}>{follower.username} - {follower.description} {<Moment fromNow>{follower.date}</Moment>}</a>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
